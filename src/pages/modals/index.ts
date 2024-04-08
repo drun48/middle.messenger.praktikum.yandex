b238f85ff8d0b4eb773b4d35692e3423ad4dd4f5
andrey <kobzev605@gmail.com>
2024-04-08 08:56:24 +0300

    refact

--- src/pages/modals/index.ts
+++ src/pages/modals/index.ts
@@ -5,29 +5,29 @@ export { default as PageModals } from "./index.hbs?raw";
 Handlebars.registerHelper("listAttach", () => [
   {
     value: "Фото или Видео",
-    photo: "/src/assets/PhotoAttach.svg",
+    photo: "./src/assets/PhotoAttach.svg",
   },
   {
     value: "Файл",
-    photo: "/src/assets/FileAttach.svg",
+    photo: "./src/assets/FileAttach.svg",
   },
   {
     value: "Локация",
-    photo: "/src/assets/LocalAttach.svg",
+    photo: "./src/assets/LocalAttach.svg",
   },
 ]);
 
 Handlebars.registerHelper("listControllerChat", () => [
   {
     value: "Добавить пользователя",
-    photo: "/src/assets/AddUser.svg",
+    photo: "./src/assets/AddUser.svg",
   },
   {
     value: "Удалить пользователя",
-    photo: "/src/assets/DeleteUser.svg",
+    photo: "./src/assets/DeleteUser.svg",
   },
   {
     value: "Удалить чат",
-    photo: "/src/assets/delete_chat.svg",
+    photo: "./src/assets/delete_chat.svg",
   },
 ]);
