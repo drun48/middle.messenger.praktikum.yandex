2337f03bbc7adb543f99d0ed4bbdadcdb6cb531f
andrey <kobzev605@gmail.com>
2024-04-08 14:18:11 +0300

    test deploy

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,18 @@ import * as Components from "./components";
 import * as Pages from "./pages";
 
 import arrow_circle from './assets/arrow-circle.svg'
-import arrow from './assets/arrow.svg'
-import attacher from './assets/attacher.svg'
-import menu from './assets/menu.svg'
-import photo_user from './assets/photoUser.png'
+// import arrow from './assets/arrow.svg'
+// import attacher from './assets/attacher.svg'
+// import menu from './assets/menu.svg'
+// import photo_user from './assets/photoUser.png'
 import profil_photo from './assets/profile_photo.svg'
 
 const pages = {
   nav: [Pages.NavPage],
   login: [Pages.LoginPage],
   signin: [Pages.PageSign],
-  listChat: [Pages.PageListChat, {arrow:arrow, attacher:attacher, menu:menu}],
-  chat: [Pages.PageChat, {arrow_circle:arrow_circle, photo_user:photo_user}],
+  // listChat: [Pages.PageListChat, {arrow:arrow, attacher:attacher, menu:menu}],
+  // chat: [Pages.PageChat, {arrow_circle:arrow_circle, photo_user:photo_user}],
   profile: [Pages.PageProfile, {arrow_circle:arrow_circle, profil_photo:profil_photo}],
   profileChanged: [Pages.PageProfileChanged, {arrow_circle:arrow_circle, profil_photo:profil_photo}],
   profileChangedPassword: [Pages.PageProfileChangedPassword, {arrow_circle:arrow_circle, profil_photo:profil_photo}],
