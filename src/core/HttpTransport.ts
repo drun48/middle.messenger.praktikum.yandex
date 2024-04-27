27513e29e8599223b360838daca324cb47a7f789
andrey <kobzev605@gmail.com>
2024-04-27 08:05:15 +0300

    refact: type for featch

--- src/core/HttpTransport.ts
+++ src/core/HttpTransport.ts
@@ -1,9 +1,11 @@
 type Options = {
   data?: Record<string, unknown>;
-  headers: Record<string, string>;
+  headers?: Record<string, string>;
   timeout?: number;
   method: string;
 };
+
+type HTTPMethod = (url: string, options?: Options) => Promise<unknown>
 
 const METHODS = {
   GET: 'GET',
@@ -24,50 +26,54 @@ function queryStringify(data: Record<string, unknown>) {
 }
 
 export class HTTPTransport {
-  GET = (url: string, options: Options) => {
+  GET:HTTPMethod = (url, options) => {
     let str = url;
-    if (options.data) {
+    if (options?.data) {
       str += queryStringify(options.data);
     }
     return this.request(
       str,
       { ...options, method: METHODS.GET },
-      options.timeout,
+      options?.timeout,
     );
   };
 
   // eslint-disable-next-line max-len
-  POST = (url: string, options: Options) => this.request(url, { ...options, method: METHODS.POST }, options.timeout);
+  POST:HTTPMethod = (url, options) => this.request(url, { ...options, method: METHODS.POST }, options?.timeout);
 
   // eslint-disable-next-line max-len
-  PUT = (url: string, options: Options) => this.request(url, { ...options, method: METHODS.PUT }, options.timeout);
+  PUT:HTTPMethod = (url, options) => this.request(url, { ...options, method: METHODS.PUT }, options?.timeout);
 
   // eslint-disable-next-line max-len
-  DELETE = (url: string, options: Options) => this.request(url, { ...options, method: METHODS.DELETE }, options.timeout);
+  DELETE:HTTPMethod = (url, options) => this.request(url, { ...options, method: METHODS.DELETE }, options?.timeout);
 
   // eslint-disable-next-line class-methods-use-this
   request = (url: string, options: Options, timeout = 5000) => {
     const { method, data, headers } = options;
+
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
+
       xhr.open(method, url);
-      Object.entries(headers).forEach(([key, value]) => {
+
+      Object.entries(headers ?? {}).forEach(([key, value]) => {
         xhr.setRequestHeader(key, value);
-        xhr.onload = function () {
-          resolve(xhr);
-        };
-
-        xhr.onabort = reject;
-        xhr.onerror = reject;
-        xhr.timeout = timeout;
-        xhr.ontimeout = reject;
-
-        if (method === 'GET' || !data) {
-          xhr.send();
-        } else {
-          xhr.send(JSON.stringify(data));
-        }
       });
+
+      xhr.onload = function () {
+        resolve(xhr);
+      };
+
+      xhr.onabort = reject;
+      xhr.onerror = reject;
+      xhr.timeout = timeout;
+      xhr.ontimeout = reject;
+
+      if (method === 'GET' || !data) {
+        xhr.send();
+      } else {
+        xhr.send(JSON.stringify(data));
+      }
     });
   };
 }
