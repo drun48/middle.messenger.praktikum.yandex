f55324fe8344be9142cd1eb2aa896c1fdef838eb
andrey <kobzev605@gmail.com>
2024-05-17 21:41:42 +0300

    fix: formateDate

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -3,7 +3,7 @@ import constants from '../constants';
 export function formatDate(dateStr:string) {
   const date = new Date(dateStr);
   return {
-    dayString: `${date.getDay()}  ${constants.MOUNTH[date.getMonth()].toLowerCase()}`,
+    dayString: `${date.getDate()}  ${constants.MOUNTH[date.getMonth()].toLowerCase()}`,
     time: `${date.getHours() > 9 ? date.getHours() : 0 + date.getHours().toString()}:${date.getMinutes() > 9 ? date.getMinutes() : 0 + date.getMinutes().toString()}`,
   };
 }
