d8c652b7818488624cdcece74ee6190a14435b1f
andrey <kobzev605@gmail.com>
2024-05-17 21:07:00 +0300

    refact: add try/catch for auth

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -10,56 +10,73 @@ import { checkStatus } from './checkStatus';
 const authApi = new AuthApi();
 
 const getUser = async () => {
-  const response = checkStatus(await authApi.getUser());
-  if (response.data) {
-    const user = response.data;
-    user.avatar = user.avatar ? constants.GET_PHOTO + user.avatar : avatar;
-    Store.set('auth', true);
-    Store.set('user', user);
-  }
-  if (response.error) {
+  try {
+    const response = checkStatus(await authApi.getUser());
+    if (response.data) {
+      const user = response.data;
+      user.avatar = user.avatar ? constants.GET_PHOTO + user.avatar : avatar;
+      Store.set('auth', true);
+      Store.set('user', user);
+    }
+    if (response.error) {
+      Store.set('auth', false);
+      Store.set('user', null);
+    }
+  } catch (e) {
     Store.set('auth', false);
     Store.set('user', null);
   }
 };
 
 const logout = async () => {
-  const response = checkStatus(await authApi.logout());
-  if (!response.error) {
-    Store.set('auth', false);
-    Router.go('/');
+  try {
+    const response = checkStatus(await authApi.logout());
+    if (!response.error) {
+      Store.set('auth', false);
+      Router.go('/');
+    }
+  } catch (e) {
+    console.log(e);
   }
 };
 
 const login = async (data:LoginDTO) => {
-  const auth = Store.getState().auth as boolean;
-  if (auth) {
-    await logout();
-  }
-  const response = checkStatus(await authApi.login(data));
-  if (response.data) {
-    Store.set('auth', true);
-    Store.set('loginError', '');
-    getUser();
-    Router.go('/messenger');
-  }
-  if (response.error) {
-    Store.set('loginError', response.error.reason);
+  try {
+    const auth = Store.getState().auth as boolean;
+    if (auth) {
+      await logout();
+    }
+    const response = checkStatus(await authApi.login(data));
+    if (response.data) {
+      Store.set('auth', true);
+      Store.set('loginError', '');
+      getUser();
+      Router.go('/messenger');
+    }
+    if (response.error) {
+      Store.set('loginError', response.error.reason);
+    }
+  } catch (e) {
+    Store.set('loginError', 'Во время входа произошла ошибка, попробуйте снова');
   }
 };
 
 const signup = async (data:SingupDTO) => {
-  const auth = Store.getState().auth as boolean;
-  if (auth) {
-    await logout();
-  }
-  const response = await authApi.signup(data);
-  if (response.data) {
-    await getUser();
-    Router.go('/messenger');
-  }
-  if (response.error) {
-    Store.set('signinError', response.error.reason);
+  try {
+    const auth = Store.getState().auth as boolean;
+    if (auth) {
+      await logout();
+    }
+    const response = await authApi.signup(data);
+    if (response.data) {
+      await getUser();
+      Router.go('/messenger');
+    }
+    if (response.error) {
+      Store.set('signinError', response.error.reason);
+    }
+  } catch (e) {
+    Store.set('signinError', 'Во время регистрации произошла ошибка, попробуйте снова');
   }
 };
 
